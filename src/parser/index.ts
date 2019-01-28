f80db5860c96ef3fcbb7dc3ae8859e9e39cf9c92
Gabriele Sato <gabri06@gmail.com>
2019-01-28 23:47:44 +0900

    add ts interface

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,1 +1,23 @@
-export default () => 'Im parser!';
+import { readFileSync } from 'fs';
+
+/**
+ * Parse from vue file
+ *
+ * @param {string} path File path
+ * @return {object} Parsed values
+ */
+function parse(path: string): Vue {
+  const regex = /(<script>(.|\n|\r)*<\/script>)/;
+  const file = readFileSync(path).toString();
+  const [scriptTemplate] = file.split(regex).filter(str => regex.test(str));
+
+  const vueObj = scriptTemplate
+    .substring(8, scriptTemplate.length - 9) // Remove the <script></script> tag
+    .split(/^export default/g)
+    .join()
+    .replace(/export default/, 'return');
+
+  return new Function(vueObj)();
+}
+
+export default parse;
---
+++ src/parser/interfaces.ts
@@ -0,0 +1,6 @@
+interface Vue {
+  data?: () => { readonly [key: string]: any };
+  props?: { readonly [key: string]: any };
+  methods?: { readonly [key: string]: () => any };
+  components?: { readonly [key: string]: Vue };
+}
