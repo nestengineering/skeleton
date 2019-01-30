b378437784a2e18b89b6eae302a021a0601b5ab8
Gabriele Sato <gabri06@gmail.com>
2019-01-30 21:39:59 +0900

    minor change

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,24 +1,31 @@
 import { readFileSync } from 'fs';
 import { Vue } from './interfaces';
+
 /**
  * Parse from vue file
  *
  * @param {string} path File path
- * @return {Vue} Parsed values
+ * @return {any} Parsed values
  */
-function parse(path: string): any {
+const parseVue = (path: string) => {
   const regex = /(<script>(.|\n|\r)*<\/script>)/;
   const file = readFileSync(path).toString();
   const [scriptTemplate] = file.split(regex).filter(str => regex.test(str));
 
   const vueObjTemplate = scriptTemplate
     .substring(8, scriptTemplate.length - 9) // Remove the <script></script> tag
-    .split(/^export default/g)
-    .join()
-    .replace(/export default/, 'return');
+    .split(/export default/g)
+    .filter(str => !/import.+("|').*("|').*/.test(str)); // Remove import statements
 
-  const vueObj: Vue = new Function(vueObjTemplate)();
-  return vueObj;
-}
+  return vueObjTemplate;
+};
 
-export default parse;
+const parseReact = () => {};
+
+const parseAngular = () => {};
+
+export default {
+  parseVue,
+  parseReact,
+  parseAngular,
+};
