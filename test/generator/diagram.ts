33eb3d73b195c6491c0c1ad484d69b37873b216d
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-01-31 01:02:39 +0900

    add generators test case

---
+++ test/generator/diagram.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+
+describe("diagram generator", () => {
+  it("case1", () => {
+    assert(1, 1);
+  });
+});
---
+++ test/generator/template.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+
+describe("template generator", () => {
+  it("case1", () => {
+    assert(1, 1);
+  });
+});
