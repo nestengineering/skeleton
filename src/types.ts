1726b1277cc18a921ecd80f70faeb2e4753338c2
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-01-30 21:23:12 +0900

    add readonly

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,14 @@
 export interface State {
-  [key: string]: any;
+  readonly [key: string]: any;
 }
 
 export interface Props {
-  [key: string]: any;
+  readonly [key: string]: any;
 }
 
 export interface Component {
-  state?: State;
-  props?: Props;
-  methods?: { [key: string]: () => any };
-  template?: Array<any>; // TODO templateは一旦スルーして、どのようにするか決める必要がある 2019/01/30
+  readonly state?: State;
+  readonly props?: Props;
+  readonly methods?: { [key: string]: () => any };
+  readonly template?: Array<any>; // TODO templateは一旦スルーして、どのようにするか決める必要がある 2019/01/30
 }
