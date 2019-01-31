449ae6a19012ee69d33ade6eff1f03b62fd046b7
gabriele.sato <gabriele.satp@persol.co.jp>
2019-01-31 18:33:02 +0900

    add template engine

--- src/generator/template.ts
+++ src/generator/template.ts
@@ -3,43 +3,31 @@ import * as fs from 'fs';
 import { Component, ComponentInfo } from './../types';
 import { Framework } from '../constants';
 
-const vueGenerator = (component: Component): string => {
-  return 'Hello world';
-};
+const engine = (
+  framework: Framework,
+  component: Component
+): { toString: () => string } =>
+  ({
+    [Framework.VUE]: { toString: () => 'Hello world' },
+    [Framework.REACT]: { toString: () => 'Hello world' },
+    [Framework.ANGULAR]: { toString: () => 'Hello world' }
+  }[framework]);
 
-const selectGenerator = (
-  framework: Framework
-): ((component: Component) => string) => {
-  return {
-    [Framework.VUE]: vueGenerator,
-    [Framework.REACT]: vueGenerator,
-    [Framework.ANGULAR]: vueGenerator
-  }[framework];
-};
-
-const writeFile = (template: ComponentInfo & { content: string }) => {
+const writeFile = (info: ComponentInfo, content: string) => {
   const output = path.resolve(
     require('os').homedir(),
-    'Desktop/skeleton/src/',
-    template.path
+    'Desktop/templates/',
+    info.path
   );
 
   fs.mkdirSync(output, { recursive: true });
-  fs.writeFileSync(
-    `${output}/${template.name}.${template.extension}`,
-    template.content
-  );
-  console.log(output);
+  fs.writeFileSync(`${output}/${info.name}.${info.extension}`, content);
 };
 
-export default {
-  createTemplate: (
-    framework: Framework,
-    component: Component & ComponentInfo
-  ) => {
-    const generator = selectGenerator(framework);
-    const { name, path, extension } = { ...component };
-    const content = generator(component);
-    writeFile({ name, path, extension, content });
-  }
-};
+const createTemplate = (
+  framework: Framework,
+  info: ComponentInfo,
+  component: Component
+) => writeFile(info, engine(framework, component).toString());
+
+export default { createTemplate };
--- test/generator/template.ts
+++ test/generator/template.ts
@@ -6,17 +6,18 @@ import { Framework } from '../../src/constants';
 
 describe('template generator', () => {
   it('It should be happy', () => {
-    const component: Component & ComponentInfo = {
-      state: {},
-      props: {},
-      methods: {},
-      templates: [],
+    const info: ComponentInfo = {
       path: '',
       extension: 'vue',
       name: 'my-first-component'
     };
-    generator.createTemplate(Framework.VUE, component);
-
+    const component: Component = {
+      state: {},
+      props: {},
+      methods: {},
+      templates: []
+    };
+    generator.createTemplate(Framework.VUE, info, component);
     assert(1, 1);
   });
 });
