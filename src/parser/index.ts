883e2c5186d396aeb67c2fb5bae700d9d62cddb5
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-02-04 00:46:48 +0900

    move parser's logic

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,54 +1,15 @@
-import { readFileSync } from 'fs';
-import * as path from 'path';
-import { Vue, Component } from './../types';
+import { Component } from './../types';
 import { Framework } from '../constants';
+import VueLogic from '../logics/vue';
+import ReactLogic from '../logics/react';
+import AngularLogic from '../logics/angular';
 
-const parsers: { [framework in Framework]: (filePath: string) => Component } = {
-  [Framework.VUE]: (filePath: string) => {
-    const regex = /(<script>(.|\n|\r)*<\/script>)/;
-    const file = readFileSync(filePath).toString();
-    const [scriptTemplate] = file.split(regex).filter(str => regex.test(str));
-
-    const template = scriptTemplate
-      .substring('<script>'.length, scriptTemplate.length - '</script>'.length)
-      .replace(/export default/g, 'return')
-      .replace(/import\s?\*?\s?(as)?{?(?=\s.*('|").*('|");)/g, 'let')
-      .replace(/\s?}?\s?from(?=\s?('|").*('|");)/g, ' =');
-
-    const vue: Vue = new Function(template)();
-    const component: Component = {
-      state: vue.data ? vue.data() : {},
-      props: vue.props ? vue.props : {},
-      methods: vue.methods ? vue.methods : {},
-      children: vue.components
-        ? Object.entries(vue.components).map(([key, relPath]) =>
-            parsers[Framework.VUE](
-              path.resolve(filePath.match(/.*(?=\/.*\.vue)/g).join(), relPath)
-            )
-          )
-        : []
-    };
-
-    return component;
-  },
-  [Framework.REACT]: (path: string) => {
-    const conponent: Component = {
-      state: {},
-      props: {},
-      methods: {},
-      children: []
-    };
-    return conponent;
-  },
-  [Framework.ANGULAR]: (path: string) => {
-    const conponent: Component = {
-      state: {},
-      props: {},
-      methods: {},
-      children: []
-    };
-    return conponent;
-  }
+const handleParsers: {
+  [framework in Framework]: (filePath: string) => Component
+} = {
+  [Framework.VUE]: VueLogic.parse,
+  [Framework.REACT]: ReactLogic.parse,
+  [Framework.ANGULAR]: AngularLogic.parse
 };
 
 /**
@@ -58,7 +19,5 @@ const parsers: { [framework in Framework]: (filePath: string) => Component } = {
  * @param path File path
  * @return Parsed values
  */
-const convert = (framework: Framework, path: string): Component =>
-  parsers[framework](path);
-
-export default { convert };
+export const parse = (framework: Framework, path: string): Component =>
+  handleParsers[framework](path);
