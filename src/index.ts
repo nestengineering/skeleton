ec5e9b7354a836b3d0536dc6cb55001cdd7e264b
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-02-04 00:46:01 +0900

    change method names

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
-import crawler from './crawler/index';
-import parser from './parser/index';
+import { parse } from './parser/index';
+import generator from './generator/index';
 import { Framework } from './constants';
 
 export default class {
   inputFW: Framework;
   constructor(input: Framework) {
     this.inputFW = input;
-  }
-  getAbstractModel(startDir) {
-    // MEMO イメージ的にはこんな感じ？
-    // return this.crawl(startDir).map(file => this.parse(file))
-    return this.crawl(startDir);
-  }
-  private crawl(startDir: string): string[] {
-    return crawler(startDir, this.inputFW);
-  }
-  private parse() {
-    // return parser();
   }
 }
