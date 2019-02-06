14fe66e5e2f8de75387f3d2f86fc7f42baad09ec
hand-dot <kyouhei.fukuda0729@gmail.com>
2019-02-06 23:36:30 +0900

    bugfix

--- src/generator/diagram.ts
+++ src/generator/diagram.ts
@@ -1,11 +1,12 @@
-import { readFileSync } from 'fs';
 import { Component } from './../types';
+import { diagramHtml } from './assets/diagram';
 
 const replacer = (k, v) => (typeof v === 'function' ? v.toString() : v);
 
 export default (data: Component) => {
-  const file = readFileSync(require.resolve('./assets/diagram.html'))
-    .toString()
-    .replace(/'SKELETON_DATA'/g, JSON.stringify(data, replacer));
+  const file = diagramHtml.replace(
+    /'SKELETON_DATA'/g,
+    JSON.stringify(data, replacer)
+  );
   return file;
 };
